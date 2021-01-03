97783b22a4dc975ac79ecc9a946d12212b72352a
Jason Dusek <jason.dusek@gmail.com>
2021-01-02 23:03:33 -0600

    Allow for shell-style or plain word vector exec (#5)
    * Allow for shell-style or plain word vector exec
    
    * Pass `-c` to the shell, to call with command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate getopts;
+extern crate simple_error;
 use getopts::Options;
+use simple_error::require_with;
+use std::cmp::min;
 use std::env;
+use std::error::Error;
 use std::process;
 
 mod runtime;
@@ -9,17 +13,8 @@ mod filesystem;
 mod mount;
 mod namespace;
 
-fn run(rootfs: &str, command_string: &str) {
-	let child_command_buffer = command_string.split(" ");
-	let mut child_command_vector = child_command_buffer.collect::<Vec<&str>>();
-	let command = child_command_vector[0];
-	child_command_vector.drain(0..1);
-
-	runtime::run_container(&rootfs, command, child_command_vector);
-}
-
 fn print_usage(program: &str, opts: &Options) {
-	let brief = format!("Usage: {} vas-quod [options]", program);
+	let brief = format!("Usage: {} vas-quod [options] [-- <command> <argument>...]", program);
 	print!("{}", opts.usage(&brief));
 }
 
@@ -29,9 +24,13 @@ fn main() {
 	let mut opts = Options::new();
 	opts.optopt("r", "rootfs", "Path to root file-system eg. --rootfs /home/alpinefs", "path");
 	opts.optopt("c", "command", "Command to be executed eg. --command `curl http://google.com`", "command");
-	opts.optflag("h", "help", "print this help menu");
+	opts.optflag("h", "help", "Print this help menu");
 
-	let matches = opts.parse(&args[1..]).ok().unwrap_or_else(|| {
+	// Find the conventional "--" that separates out remaing arguments.
+	let end_processable = args.iter().position(|s| s == "--").unwrap_or_else(|| args.len());
+	let begin_unprocessable = min(end_processable + 1, args.len());
+
+	let matches = opts.parse(&args[1..end_processable]).ok().unwrap_or_else(|| {
 		println!("Error: Unrecognzied options");
 		print_usage(&program, &opts);
 		process::exit(7);
@@ -44,15 +43,43 @@ fn main() {
 	}
 
 	let rootfs = matches.opt_str("r").unwrap_or_else(|| {
-		println!("Error: Please pass: --rootfs");
-		print_usage(&program, &opts);
-		process::exit(7);
-	});
-	let command_string = matches.opt_str("c").unwrap_or_else(|| {
-		println!("Error: Please pass: --command");
+		println!("Error: Please pass `--rootfs <path>`");
 		print_usage(&program, &opts);
 		process::exit(7);
 	});
 
-	run(&rootfs, &command_string);
+	let c = matches.opt_str("c"); // NB: Seperate let binding for lifetime.
+	let (command, args) = determine_command_tuple(&c, &args[begin_unprocessable..args.len()]).ok().unwrap_or_else(|| {
+		println!("Error: Please pass `--command <shell command>` or `-- <command> <argument>...`");
+		print_usage(&program, &opts);
+		process::exit(7);
+	});
+
+	runtime::run_container(&rootfs, command, args);
+}
+
+/// Determines based on the inputs whether we are going to invoke a shell, with
+/// shell interpretation, or a simple unescaped argument vector.
+/// Rearranges arguments as needed, but doesn't reallocate.
+fn determine_command_tuple<'a, T: AsRef<str> + 'a>(shell_command: &'a Option<T>, argv: &'a [T]) -> Result<(&'a str, Vec<&'a str>), Box<dyn Error>> {
+	let mut vec: Vec<&str> = vec![];
+
+	// Prepend shell and command string if a command string is given.
+	if let Some(shell_command) = shell_command {
+		vec.push("/bin/sh");
+		vec.push("-c");
+		vec.push(shell_command.as_ref());
+	}
+
+	// The args given will be the whole command if there's no shell string;
+	// otherwise, they'll be added to the argument vector.
+	vec.extend(argv.iter().map(|item| item.as_ref()));
+
+	// Shift off the first word as the command, erroring out if no command is
+	// given.
+	vec.reverse();
+	let command = require_with!(vec.pop(), "Empty command!");
+	vec.reverse();
+
+	return Ok((command, vec));
 }
