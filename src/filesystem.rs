436162b94e5a3d2d39960d97826db4d6833d904f
flouthoc <flouthoc.git@gmail.com>
2021-01-24 13:32:35 +0530

    Filesystem isolation fix: Switch to pivot_root for mounting rootfs

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,8 +1,17 @@
+use std::fs;
+use std::path::PathBuf;
 use nix::unistd;
+use nix::sys::stat;
+use nix::{Error, Result};
 
 static ROOT_PATH: &str = "/";
+static OLD_ROOT_PATH: &str = ".oldroot";
 
 pub fn set_root_fs(rootfs: &str){
-	unistd::chroot(rootfs).unwrap();
-	let _status = unistd::chdir(ROOT_PATH);
+	
+	let p_root_fs = PathBuf::from(rootfs).join(OLD_ROOT_PATH);
+	let _rm_status = fs::remove_dir_all(&p_root_fs).map_err(|_| Error::InvalidPath);
+	let _mkdir_status = unistd::mkdir(&p_root_fs, stat::Mode::S_IRWXU | stat::Mode::S_IRWXG | stat::Mode::S_IRWXO,);
+	let _pivot_root_status = unistd::pivot_root(rootfs, &p_root_fs);
+	let _chdir_status = unistd::chdir(ROOT_PATH);
 }
--- src/mount.rs
+++ src/mount.rs
@@ -1,10 +1,19 @@
 use nix::mount;
 
 static PROC: &str = "proc";
+static OLD_ROOT_PATH: &str = "/.oldroot";
 
 pub fn mount_proc(){
 	const NONE: Option<&'static [u8]> = None;
 	mount::mount(Some(PROC), PROC, Some(PROC), mount::MsFlags::empty(), NONE).expect("Failed to mount the /proc");
+}
+
+pub fn mount_root_fs(rootfs: &str){
+	let _status = mount::mount(Some(rootfs),rootfs,None::<&str>,mount::MsFlags::MS_BIND | mount::MsFlags::MS_REC,None::<&str>,);
+}
+
+pub fn unmount_host_root_fs(){
+	let _status = mount::umount2(OLD_ROOT_PATH, mount::MntFlags::MNT_DETACH);
 }
 
 pub fn unmount_proc(){
