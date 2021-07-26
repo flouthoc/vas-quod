8be1669f79293cb809a81c18858b2a08c9a83de3
Tim Boudreau <tim@timboudreau.com>
2021-07-26 01:08:13 -0400

    Exit with status code of child process; ensure `unmount_proc()` runs regardless of what else happens.

--- src/run.rs
+++ src/run.rs
@@ -14,22 +14,24 @@ use nix::sys::stat;
 use nix::Error;
 
 use crate::runtime;
+use std::cell::RefCell;
+use nix::sys::wait::WaitStatus;
 
 // TODO: Add RunOpts and use that instead of individual args
 
-pub fn run(config: &str, bundle: &str) {
+pub fn run(config: &str, bundle: &str) -> nix::Result<WaitStatus> {
 	let mut base_config: &str = "config.json";
-	if(!config.is_empty()){
+	if !config.is_empty() {
 		base_config = config;
 	}
-	
+
 	let spec: libocispec::runtime::Spec = match libocispec::runtime::Spec::load(base_config) {
 		Ok(spec) => spec,
 		Err(e) => panic!("{}", e),
 	};
-	
-	// TODO: Implement core	
-	runtime::run_container(&spec.root.as_ref().unwrap().path, 
+
+	// TODO: Implement core
+	runtime::run_container(&spec.root.as_ref().unwrap().path,
 				spec.process.as_ref().unwrap().args.as_ref().unwrap().clone().into_iter().nth(0).as_ref().unwrap(), 
-				[].to_vec());
+				[].to_vec())
 }
